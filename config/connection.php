d26834492afa762e0a0a83b99fecce538d78cb49
Miftah Thoha <thohadi223@gmail.com>
2018-02-11 01:40:10 +0700

    reset root password config

--- config/connection.php
+++ config/connection.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli = new mysqli("127.0.0.1", "root", "thohadi223", "pemilu");
+	$mysqli = new mysqli("127.0.0.1", "root", "", "pemilu");
 
 	if (mysqli_connect_errno()) {
 		printf("Koneksi ke database gagal!: %s\n", mysqli_connect_error());
